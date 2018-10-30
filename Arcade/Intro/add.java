1e49fc5f2738efc08040e03da7625f77412a7e62
Maria <maria.clr.dias@gmail.com>
2018-10-30 19:28:21 -0200

    Soma Java

--- Arcade/Intro/add.java
+++ Arcade/Intro/add.java
@@ -1,3 +1,11 @@
 class add{
 
+  public static void soma(int a, int b){
+    return a+b;
+  }
+
+  public static void main(String[] args) {
+    int a=9, b=7;
+    System.out.println(soma(a,b));
+  }
 }
